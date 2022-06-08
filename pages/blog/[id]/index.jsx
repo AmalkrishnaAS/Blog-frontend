70fa8355dbb5b0a044827fd922e7fb180ecfb4f7
Amalkrishna A S <amalkrishna02016@gmail.com>
2022-06-08 10:25:02 +0530

    quill changes

--- pages/blog/[id]/index.jsx
+++ pages/blog/[id]/index.jsx
@@ -33,18 +33,19 @@ const index = ({blog,comments}) => {
     
     
   return (
-    <div class="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16 relative">
+    <div class=" w-screen mx-auto p-5 sm:p-10 md:p-16 relative">
        
         
-  <div class="max-w-2xl mx-auto">
+  <div class=" mx-auto">
   <img className='m-auto max-h-[450px] ' src={blog.thumbnail} alt="" />
     <div class="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
         <div class="">
               <div href="#" class="text-xs text-indigo-600 uppercase font-medium ">
               Last Update: {moment(blog.updated_at).fromNow()}
               </div>
-              <h1 href="#" class="text-gray-900 font-bold text-3xl mb-2">{blog.title}</h1>
               <div class="text-gray-700 text-xs mt-2">Written By: <span href="#" class="text-indigo-600 font-medium  ease-in-out capitalize">
+              <h1  href="#" class="text-gray-900 font-bold text-3xl mb-2">{blog.title}</h1>
+             
                 {blog.Author}
               </span></div>
                <div className='ql-snow ' ><div className='ql-editor '>{ReactHtmlParser(blog.content)}</div></div>
--- pages/update/[id]/index.jsx
+++ pages/update/[id]/index.jsx
@@ -84,11 +84,11 @@ function App({data}) {
 
 <div class="mb-6">
     <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 ">Title</label>
-    <input value={formdata.title} type="text" id="base-input" name="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " onChange={handleChange} required />
+    <input value={formdata.title} type="text" id="base-input" name="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:border-blue-500 block w-full p-2.5 " onChange={handleChange} required />
 </div>
 <div class="mb-6">
     <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 ">Thumbnail URL</label>
-    <input value={data.thumbnail} type="text" name="thumbnail" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " onChange={handleChange} />
+    <input value={data.thumbnail} type="text" name="thumbnail" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:border-blue-500 block w-full p-2.5 " onChange={handleChange} />
 </div>
 <div class="mb-3">
     <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 ">Content</label>
@@ -102,11 +102,22 @@ function App({data}) {
             
             ['bold', 'italic', 'underline', 'strike', 'blockquote'],
             [{header: 1}, {header: 2}],
+            //font sans
+            [{ size: ['small', false, 'large', 'huge'] }],
+            [{ color: [] }, { background: [] }],
+            [{script: 'sub'}, {script: 'super'}],
+            [{indent: '-1'}, {indent: '+1'}],
+            
+            [{header: [ 3, 4, 5, 6,false]}],
+            [{font: []}],
+
+
+            
 
            
             [ 'code-block'],
             [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
-            [{ 'align': [] }],
+            [{ 'align': 'center' },{ 'align': 'right' },{ 'align': 'justify' }],
             ['link','video','image'],
           
 
@@ -127,12 +138,12 @@ function App({data}) {
       
       <label for="default-toggle" class="inline-flex relative items-center cursor-pointer">
       <input type="checkbox" value={publish} checked={publish}  id="default-toggle" class="sr-only peer" onClick={()=>setPublish(!publish)} />
-      <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none   rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+      <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none   rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-blue-600"></div>
       <span class="ml-3 text-sm font-medium text-gray-900 ">Publish</span>
       </label>
       </div>
     <div class="">
-    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800  focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
 </div>
 
 
